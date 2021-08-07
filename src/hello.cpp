a8d2defaefd7e0ea456785dbaf26452325ec5039
Alecto <perez.cs@pm.me>
2021-08-07 03:43:40 -0600

    Created first working coroutine

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <parser.hpp>
 
+noam::do_parse<int> get_int() {
+    co_return 3;
+}
 int main() {
-    puts("Hello, world!");
+    printf("Value: %i\n", get_int().run(std::string_view{}).value());
 }
