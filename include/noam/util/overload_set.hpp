aeda838f51824d72d653842d59f1a23cf64796f9
Alecto <perez.cs@pm.me>
2021-08-08 19:21:09 -0600

    Added noam::overload_set
    This allows you to easily combine multiple lambdas into a single callable overload set

---
+++ include/noam/util/overload_set.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace noam {
+/**
+ * @brief overload_set<F...> is a functor whose overload set is the union of the
+ * overload sets of { F::operator()... }
+ *
+ * @tparam Functions Defines the types of the functors used to construct this overload set
+ */
+template <class... Functions>
+struct overload_set : Functions... {
+    using Functions::operator()...;
+};
+template <class... Functions>
+overload_set(Functions...) -> overload_set<Functions...>;
+} // namespace noam
