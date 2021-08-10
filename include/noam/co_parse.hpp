5ea48162e9a0ecbff50c4f5aec197a32f0f2eaa2
Alecto Irene Perez <perez.cs@pm.me>
2021-08-10 02:01:39 -0600

    Redefined noam::co_parse<T> as a type alias to noam::parser specialization parser<parse_promise<T>>

--- include/noam/co_parse.hpp
+++ include/noam/co_parse.hpp
@@ -3,25 +3,26 @@
 #include <noam/coupling/parse_promise.hpp>
 #include <noam/result_types.hpp>
 #include <noam/util/stdlib_coroutine.hpp>
+#include <noam/parser.hpp>
 #include <optional>
 #include <string_view>
 
 namespace noam {
 template <class T>
-class co_parse {
+class parser<parse_promise<T>> {
     using handle_t = std::coroutine_handle<parse_promise<T>>;
     handle_t handle_;
 
    public:
     using promise_type = parse_promise<T>;
-    co_parse() = default;
-    co_parse(co_parse&& d) noexcept
+    parser() = default;
+    parser(parser&& d) noexcept
       : handle_(std::exchange(d.handle_, nullptr)) {}
-    co_parse(std::coroutine_handle<promise_type> handle) noexcept
+    parser(std::coroutine_handle<promise_type> handle) noexcept
       : handle_(handle) {}
 
     // This should only ever be run once, so it must be run either
-    // on a prvalue co_parse or a moved co_parse
+    // on a prvalue parser or a moved parser
     standard_result<T> operator()(state_t state) && {
         handle_.promise().set_initial_state(state);
         handle_.resume();
@@ -32,6 +33,9 @@ class co_parse {
         handle_.resume();
         return std::move(handle_.promise()).get_parse_result();
     }
-    ~co_parse() { handle_.destroy(); }
+    ~parser() { handle_.destroy(); }
 };
+
+template <class T>
+using co_parse = parser<parse_promise<T>>;
 } // namespace noam
