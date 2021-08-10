ac251756e4123ac62176754e3c78b21ef1da00a7
Alecto Irene Perez <perez.cs@pm.me>
2021-08-10 13:03:12 -0600

    Move noam::result_traits into concepts.hpp
    These aren't results themselves, and they're important to the definitions of some concepts that are going to be added in the future, like the upcoming noam::always_good_result

--- include/noam/concepts.hpp
+++ include/noam/concepts.hpp
@@ -49,4 +49,26 @@ template <class Parser>
 concept any_parser = requires(Parser parser, state_t state) {
     { parser.parse(state) } -> parse_result;
 };
+
+template <class Result>
+struct result_traits {
+    // Individual Result types can certify that good() always returns true
+    // but by default it is unknown if result.good() always returns true
+    // Used to certify a result type as a Good Boy 🐶
+    constexpr static bool always_good = false;
+};
+
+// Decay qualified types, so that we can quickly check if those are Good Boys 🐶
+template <qualified_type Result>
+struct result_traits<Result> : result_traits<std::decay_t<Result>> {
+    using base = result_traits<std::decay_t<Result>>;
+    using base::always_good;
+};
+
+// Indicates that a result is a Good Boy 🐶
+template <class Result>
+constexpr bool result_always_good_v = result_traits<Result>::always_good;
+
+template <class Result>
+constexpr bool result_is_🐶 = result_traits<Result>::always_good;
 } // namespace noam
