499eef4fdbf64b5dfa79f76269348cbe65738935
Alecto Irene Perez <perez.cs@pm.me>
2021-08-10 00:53:54 -0600

    Simplified implementation of read_int example

--- src/hello.cpp
+++ src/hello.cpp
@@ -4,36 +4,9 @@
 #include <noam/intrinsics.hpp>
 #include <vector>
 
-bool is_digit(char c) { return '0' <= c && c <= '9'; }
-
-auto get_state = noam::parser {
-    [](std::string_view state) { return noam::state_result {state}; }};
-
-auto get_char =
-    noam::parser {[](std::string_view state) -> noam::standard_result<char> {
-        return state.empty()
-                 ? noam::standard_result<char> {}
-                 : noam::standard_result<char> {state.substr(1), state[0]};
-    }};
-
-auto get_digit =
-    noam::parser {[](std::string_view state) -> noam::standard_result<int> {
-        if (state.empty()) {
-            return {};
-        } else {
-            char c = state[0];
-
-            return is_digit(c)
-                     ? noam::standard_result<int> {state.substr(1), c - '0'}
-                     : noam::standard_result<int> {};
-        }
-    }};
-
-noam::co_parse<long> read_int() {
-    auto fold_digit = [](long a, long b) { return a * 10 + b; };
-    int value = co_await noam::fold_left(get_digit, fold_digit);
-    co_return value;
-}
+constexpr auto read_int = []() -> noam::co_parse<long> {
+    co_return co_await noam::parse_long;
+};
 
 int main() {
     std::string_view str = "93289 hello";
