f76e8fce0748b74eae43deed12dc509c82adf338
Alecto Irene Perez <perez.cs@pm.me>
2021-08-10 02:07:06 -0600

    Improved documentation of parser<parse_promise<T>>.parse

--- include/noam/co_parse.hpp
+++ include/noam/co_parse.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include <noam/concepts.hpp>
 #include <noam/coupling/parse_promise.hpp>
+#include <noam/parser.hpp>
 #include <noam/result_types.hpp>
 #include <noam/util/stdlib_coroutine.hpp>
-#include <noam/parser.hpp>
 #include <optional>
 #include <string_view>
 
@@ -21,13 +21,17 @@ class parser<parse_promise<T>> {
     parser(std::coroutine_handle<promise_type> handle) noexcept
       : handle_(handle) {}
 
-    // This should only ever be run once, so it must be run either
-    // on a prvalue parser or a moved parser
-    standard_result<T> operator()(state_t state) && {
-        handle_.promise().set_initial_state(state);
-        handle_.resume();
-        return std::move(handle_.promise()).get_parse_result();
-    }
+    /**
+     * @brief Parses the input state, returning a standard_result<T>
+     *
+     * This should only ever be run once, so it must be run either on a prvalue
+     * parser or a moved parser. TODO: Fix this by specializing parse_promise so
+     * that it can produce a new instance of the coroutine frame that's been
+     * reset back to the initial state of the parser
+     *
+     * @param state the string or substring being parsed
+     * @return standard_result<T>
+     */
     standard_result<T> parse(state_t state) && {
         handle_.promise().set_initial_state(state);
         handle_.resume();
