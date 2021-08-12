91fe77fb343e004c10bec678f0972c6e11c745a7
Alecto Irene Perez <perez.cs@pm.me>
2021-08-12 17:11:51 -0600

    Added explanation + made code cleaner

--- src/hello.cpp
+++ src/hello.cpp
@@ -6,15 +6,23 @@
 
 constexpr noam::parser parse_longs =
     [](noam::state_t) -> noam::co_result<std::vector<long>> {
+    using noam::parse_long;
+    using noam::parser;
+    using noam::parse_comma_separator;
+    using noam::try_parse;
+
     std::vector<long> vect;
 
     // Get the first value
-    vect.push_back(co_await noam::parse_long);
+    vect.push_back(co_await parse_long);
 
     // (>>) : Parser a -> Parser b -> Parser b
     // noam::try_parse : Parser a -> Parser Maybe a
-    noam::parser next_value =
-        noam::try_parse(noam::parse_comma_separator >> noam::parse_long);
+    // NB: parse_comma_separator reads whitespace on either side of the comma
+    // so (whitespace >> parse_constexpr_prefix<','> >> whitespace)
+    //      === parse_separator<','>
+    //      === parse_comma_separator
+    parser next_value = try_parse(parse_comma_separator >> parse_long);
 
     // While there's a value followed by a comma, add it to the vector
     while (std::optional<long> value = co_await next_value) {
