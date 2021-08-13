a39d2c2253f0b6ea972a6689a31d7f6f07a0d5d6
Alecto Irene Perez <perez.cs@pm.me>
2021-08-12 23:41:30 -0600

    Further consistency improvements for combinators

--- include/noam/combinators.hpp
+++ include/noam/combinators.hpp
@@ -15,15 +15,14 @@ namespace noam {
  */
 template <class Value>
 constexpr auto pure(Value&& value) {
-    return noam::parser {
-        [value = std::forward<Value>(value)](std::string_view state) {
-            return noam::pure_result {state, value};
-        }};
-};
+    return [value = std::forward<Value>(value)](std::string_view state) {
+        return noam::pure_result {state, value};
+    } / make_parser;
+}
 
 /**
- * @brief Creates a parser parser and returns a parser that reads values parsed by parser
- * and combines them using fold
+ * @brief Creates a parser parser and returns a parser that reads values parsed
+ * by parser and combines them using fold
  *
  * @param parser the parser to fold
  * @param fold the operation with which to combine values produced by parser
@@ -33,7 +32,7 @@ constexpr auto pure(Value&& value) {
 template <class Parser, class Op>
 constexpr auto fold_left(Parser&& parser, Op&& fold) {
     using value_t = parser_value_t<Parser>;
-    return noam::parser {
+    return
         [parser = std::forward<Parser>(parser), fold = std::forward<Op>(fold)](
             state_t state) -> noam::standard_result<value_t> {
             auto result = parser.parse(state);
@@ -48,8 +47,8 @@ constexpr auto fold_left(Parser&& parser, Op&& fold) {
                 state = result.get_state();
             }
             return noam::standard_result {state, value};
-        }};
-};
+        } / make_parser;
+}
 
 /**
  * @brief Maps a function over a parser, returning a new parser whose output is
@@ -64,77 +63,79 @@ constexpr auto fold_left(Parser&& parser, Op&& fold) {
  */
 template <class Func, any_parser Parser>
 auto map(Func&& func, Parser&& parser) {
-    return noam::parser {[f = std::forward<Func>(func),
-                    parser = std::forward<Parser>(parser)](state_t state) {
+    return [f = std::forward<Func>(func),
+            parser = std::forward<Parser>(parser)](state_t state) {
         return transform_result {parser.parse(state), f};
-    }};
+    };
 }
 
 /**
- * @brief Takes a parser parser and produces a new parser that generates a true if parser
- * succeeded and false if parser failed
+ * @brief Takes a parser parser and produces a new parser that generates a true
+ * if parser succeeded and false if parser failed
  *
  * @param parser A parser whose output you wish to test
  * @return parser<(lambda)> A new parser
  */
 template <class Parser>
 constexpr auto test(Parser&& parser) {
-    return noam::parser {
+    return
         [parser = std::forward<Parser>(parser)](state_t sv) -> boolean_result {
-            // Note that boolean_result will select parser(sv).get_state() if parser(sv)
-            // is good, and as a result no check needs to be done here
+            // Note that boolean_result will select parser(sv).get_state() if
+            // parser(sv) is good, and as a result no check needs to be done
+            // here
             return boolean_result(sv, parser.parse(sv));
-        }};
-};
+        } / make_parser;
+}
 
 /**
- * @brief Takes `parser` and produces a new parser that generates true if the
- * parser succeeds, and false if the parser fails. If the parser succeeded, func
- * is invoked with parser.parse(...).get_value().
+ * @brief Takes `parser` and produces a new parser that generates true if
+ * the parser succeeds, and false if the parser fails. If the parser
+ * succeeded, func is invoked with parser.parse(...).get_value().
  *
  *
  * @param parser The parser to test
- * @param func the func to call on the value produced by the parser, when good
+ * @param func the func to call on the value produced by the parser, when
+ * good
  * @return parser A
  */
 template <class Parser, class Func>
 constexpr auto test_then(Parser&& parser, Func&& func) {
-    return noam::parser {
-        [parser = std::forward<Parser>(parser),
-         func = std::forward<Func>(func)](state_t state) -> boolean_result {
-            auto result = parser.parse(state);
-            if (result.good()) {
-                state = result.get_state();
-                func(std::move(result).get_value());
-                return boolean_result(state, true);
-            } else {
-                return boolean_result(state, false);
-            }
-        }};
-};
+    return [parser = std::forward<Parser>(parser),
+            func = std::forward<Func>(func)](state_t state) -> boolean_result {
+        auto result = parser.parse(state);
+        if (result.good()) {
+            state = result.get_state();
+            func(std::move(result).get_value());
+            return boolean_result(state, true);
+        } else {
+            return boolean_result(state, false);
+        }
+    } / make_parser;
+}
 
 /**
- * @brief Takes a parser parser and produces a new parser that generates a true if parser
- * succeeded and false if parser failed. Does so with lookahead, so that no portion
- * of the string is consumed
+ * @brief Takes a parser parser and produces a new parser that generates a
+ * true if parser succeeded and false if parser failed. Does so with
+ * lookahead, so that no portion of the string is consumed
  *
  * @param parser A parser whose output you wish to test
  * @return parser<(lambda)> A new parser
  */
 template <class Parser>
 constexpr auto test_lookahead(Parser&& parser) {
-    return noam::parser {[parser = std::forward<Parser>(parser)](state_t sv) -> boolean_result {
-        return boolean_result(sv, parser.parse(sv).good());
-    }};
-};
+    return
+        [parser = std::forward<Parser>(parser)](state_t sv) -> boolean_result {
+            return boolean_result(sv, parser.parse(sv).good());
+        } / make_parser;
+}
 
 /**
- * @brief Creates a parser that returns true if a prefix is matched and false
- * otherwise
+ * @brief Creates a parser that returns true if a prefix is matched and
+ * false otherwise
  *
  * @param prefix the prefix to test
  */
-constexpr auto test_prefix = [](std::string_view prefix) {
+constexpr auto test_prefix(std::string_view prefix) {
     return [=](noam::state_t state) {
         if (state.starts_with(prefix)) {
             return boolean_result {state.substr(prefix.size()), true};
@@ -142,7 +143,7 @@ constexpr auto test_prefix = [](std::string_view prefix) {
             return boolean_result {state, false};
         }
     } / make_parser;
-};
+}
 
 /**
  * @brief Creates a parser that requires a prefix. It'll return the prefix
@@ -151,7 +152,7 @@ constexpr auto test_prefix = [](std::string_view prefix) {
  * @param prefix input to match
  *
  */
-constexpr auto require_prefix = [](std::string_view prefix) {
+constexpr auto require_prefix(std::string_view prefix) {
     return [=](noam::state_t state) -> standard_result<std::string_view> {
         if (state.starts_with(prefix)) {
             return {state.substr(prefix.size()), prefix};
@@ -159,7 +160,7 @@ constexpr auto require_prefix = [](std::string_view prefix) {
             return {};
         }
     } / make_parser;
-};
+}
 
 /**
  * @brief Parses a value with lookahead, so that no part of the string is
@@ -174,10 +175,10 @@ constexpr auto lookahead(Parser&& parser) {
     return [parser = std::forward<Parser>(parser)](state_t state) {
         result_t result = parser.parse(state);
 
-        // If the result is lookahead_enabled, we can simply reset the state of
-        // the result without transforming it's type. Otherwise, we'll transform
-        // it into either a pure_result (if it's always good), or a
-        // standard_result (if it may not always be good)
+        // If the result is lookahead_enabled, we can simply reset the state
+        // of the result without transforming it's type. Otherwise, we'll
+        // transform it into either a pure_result (if it's always good), or
+        // a standard_result (if it may not always be good)
         if constexpr (lookahead_enabled_result<result_t>) {
             result.set_state(state);
             return result;
@@ -192,7 +193,7 @@ constexpr auto lookahead(Parser&& parser) {
             }
         }
     } / make_parser;
-};
+}
 
 /**
  * @brief Takes a Parser a and returns Parser Maybe a (which will always
@@ -210,11 +211,11 @@ constexpr auto try_parse(Parser&& parser) {
             result_good ? std::optional {std::move(result).get_value()}
                         : std::nullopt};
     } / make_parser;
-};
+}
 
 /**
- * @brief Attempts to parse a value, but does so with lookahead (so no part of
- * the string is consumed)
+ * @brief Attempts to parse a value, but does so with lookahead (so no part
+ * of the string is consumed)
  *
  * @param parser the parser being given to the combinator to be transformed
  */
@@ -223,9 +224,10 @@ constexpr auto try_lookahead(Parser&& parser) {
     return [parser = std::forward<Parser>(parser)](state_t state) {
         auto result = parser.parse(state);
         return pure_result {
-            state, // Because we're doing lookahead, state doesn't get updated
+            state, // Because we're doing lookahead, state doesn't get
+                   // updated
             result.good() ? std::optional {std::move(result).get_value()}
                           : std::nullopt};
     } / make_parser;
-};
+}
 } // namespace noam
