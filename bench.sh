32de66fcfecac44599d0c7b57486e87c196a4bfd
Alecto Irene Perez <perez.cs@pm.me>
2021-08-15 03:35:59 -0600

    Added bench.sh, which builds and then benchmarks the project. Data is placed in bench/data/<commit>.csv

---
+++ bench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+commit="$(git rev-parse --short=12 HEAD)"
+project_root="$(dirname $0)"
+cd "$project_root"
+
+bash build.sh
+build/bench \
+    --benchmark_min_time=0.1 \
+    --benchmark_repetitions=100 \
+    --benchmark_enable_random_interleaving=true \
+    --benchmark_display_aggregates_only=true \
+    --benchmark_out_format=csv \
+    --benchmark_out="bench/data/${commit}.csv"
