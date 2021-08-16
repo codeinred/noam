3cbe56f282ecdd610efe367f2f5c94c1fe35dae9
Alecto Irene Perez <perez.cs@pm.me>
2021-08-15 19:41:13 -0600

    bench.sh forwards arguments to cmake now

--- bench.sh
+++ bench.sh
@@ -3,7 +3,7 @@ commit="$(git rev-parse --short=12 HEAD)"
 project_root="$(dirname $0)"
 cd "$project_root"
 
-bash build.sh
+bash build.sh "${@}"
 build/bench \
     --benchmark_min_time=0.1 \
     --benchmark_repetitions=100 \
