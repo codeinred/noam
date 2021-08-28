e02675d064b5b5a036ada2997030355d9157768d
Alecto <perez.cs@pm.me>
2021-08-28 02:40:39 -0600

    Implemented make combinator
    This combinator will give you a parser that will construct a given type out of the values parsed by the parsers given as inputs to make

--- include/noam/combinators.hpp
+++ include/noam/combinators.hpp
@@ -292,4 +292,80 @@ constexpr auto try_lookahead(Parser&& parser) {
                    : std::nullopt};
     } / make_parser;
 }
+
+/**
+ * @brief Attempts to apply a parser, placing the result in r. On success,
+ * returns true and updates the state. On failure, returns false, and the state
+ * is not updated.
+ *
+ * @tparam P the type of the parser being passed to parse_asign
+ * @tparam Result the type of the result that will be assigned the output of
+ * parser.parse(st)
+ * @param st the state
+ * @param parser the parser to apply to the state
+ * @param r the result which will be assigned the output of parser.parse(st)
+ * @return true if the parser succeeded
+ * @return false if the parser failed
+ */
+template <class P, class Result>
+constexpr bool parse_assign(state_t& st, P const& parser, Result& r) {
+    if constexpr (parser_always_good_v<P>(parser)) {
+        r = parser.parse(st);
+        st = r.get_state();
+        return true;
+    } else {
+        if ((r = parser.parse(st))) {
+            st = r.get_state();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * @brief Make combinator. Creates a parser that will construct a object of type
+ * T using arguments obtained by applying parsers p1...pn in succession
+ *
+ * @tparam T
+ * @tparam P
+ * @param parsers
+ * @return auto
+ */
+template <class T, class... P>
+auto make(P&&... parsers) {
+    return parser {
+        [... parsers = std::forward<P>(parsers)](state_t st) -> result<T> {
+            return [&](auto... results) -> result<T> {
+                if ((parse_assign(st, parsers, results) && ...)) {
+                    return {st, T {std::move(results).get_value()...}};
+                } else {
+                    return {};
+                }
+            }(default_constructible_parser_result_t<P> {}...);
+        }};
+}
+
+/**
+ * @brief Make combinator. Creates a parser that will construct a object whose
+ * type is deduced by invoking T with arguments obtained by applying parsers
+ * p1...pn in succession
+ *
+ * @tparam T
+ * @tparam P
+ * @param parsers
+ * @return auto
+ */
+template <template <class...> class T, class... P>
+auto make(P&&... parsers) {
+    return parser {
+        [... parsers = std::forward<P>(parsers)](state_t st) {
+            return [&](auto... results) {
+                return (parse_assign(st, parsers, results) && ...)
+                    ? result {st, T {std::move(results).get_value()...}}
+                    : null_result;
+            }(default_constructible_parser_result_t<P> {}...);
+        }};
+}
+
 } // namespace noam
