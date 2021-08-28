db86ed0edd146252a078eb924812130906aa9eed
Alecto Irene Perez <perez.cs@pm.me>
2021-08-28 01:20:02 -0600

    Added make_w_coro, which is a really clean implementation of the make combinator using coroutines

--- include/noam/co_await.hpp
+++ include/noam/co_await.hpp
@@ -15,3 +15,24 @@ template <class T>
 struct std::coroutine_traits<noam::result<T>, noam::state_t> {
     using promise_type = noam::parse_promise<T>;
 };
+
+namespace noam {
+/**
+ * @brief Takes a parsers p1...pn and produces a parser that returns a value of
+ * T constructed from the values parsed by p1...pn.
+ *
+ * @tparam T the type the parser should construct
+ * @tparam Parsers the types of the parsers used to obtain the arguments to T's
+ * constructor
+ * @param parsers the parsers used to obtain the arguments to T's constructor
+ * @return parser returns a parser
+ */
+template <class T, class... Parsers>
+constexpr auto make_w_coro(Parsers&&... parsers) {
+    return parser {
+        [... parsers =
+             std::forward<Parsers>(parsers)](state_t st) -> noam::result<T> {
+            co_return T {(co_await parsers)...};
+        }};
+}
+} // namespace noam
