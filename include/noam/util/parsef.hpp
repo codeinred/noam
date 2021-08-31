a830bebe0e7f77264628f3a90bab828c26c2631d
Alecto Irene Perez <perez.cs@pm.me>
2021-08-30 18:08:15 -0600

    Added noam::parsef::match, noam::parsef::join

--- include/noam/util/parsef.hpp
+++ include/noam/util/parsef.hpp
@@ -5,6 +5,7 @@
 #include <noam/result_types.hpp>
 #include <noam/type_traits.hpp>
 #include <noam/util/helpers.hpp>
+#include <tuplet/tuple.hpp>
 
 // This file holds functios that return parsers based on inputs
 
@@ -68,4 +69,55 @@ struct surround {
 };
 template <class A, class B, class C>
 surround(A, B, C) -> surround<A, B, C>;
+
+template <class... Parsers>
+struct match : tuplet::tuple<Parsers...> {
+    using base = tuplet::tuple<Parsers...>;
+    using base_list = typename base::base_list;
+    constexpr static bool always_good = (parser_always_good_v<Parsers> && ...);
+    using result_type =
+        std::conditional_t<always_good, pure_result<empty>, result<empty>>;
+    template <class... Bases>
+    constexpr auto parse_impl(state_t st, tuplet::type_list<Bases...>) const
+        -> result_type {
+        if constexpr (always_good) {
+            (update_state(Bases::value.parse(st), st), ...);
+            return {st, empty {}};
+        } else {
+            if ((update_state(Bases::value.parse(st), st) && ...)) {
+                return {st, empty {}};
+            } else {
+                return {};
+            }
+        }
+    }
+    constexpr auto operator()(state_t st) const -> result_type {
+        return parse_impl(st, base_list{});
+    }
+};
+
+template <class... P>
+struct join : meta::all_but_last_t<match, P...> {
+    using match_t = meta::all_but_last_t<match, P...>;
+    using last_parser = meta::last_t<P...>;
+    using result_type = std::conditional_t<
+        match_t::always_good,
+        parser_result_t<last_parser>,
+        result<parser_value_t<last_parser>>>;
+    last_parser p;
+    template <class... Bases>
+    constexpr auto operator()(state_t st) const -> result_type {
+        if constexpr (match_t::always_good) {
+            return p.parse(match_t::parse_impl(st, typename match_t::base_list{}).get_state());
+        } else {
+            if (auto r = match_t::parse_impl(st, typename match_t::base_list{})) {
+                return p.parse(r.get_state());
+            } else {
+                return {};
+            }
+        }
+    }
+};
+template <class... T>
+join(T...) -> join<T...>;
 } // namespace noam::parsef
