2ed1cc38efda3953dee0b8fceefd3f17ee0d1feb
Alecto Irene Perez <perez.cs@pm.me>
2021-08-30 18:07:21 -0600

    Added noam::update_state, noam::meta::*

--- include/noam/util/helpers.hpp
+++ include/noam/util/helpers.hpp
@@ -2,6 +2,20 @@
 #include <noam/type_traits.hpp>
 
 namespace noam {
+template <class Result>
+constexpr bool update_state(Result const& r, state_t& st) {
+    if constexpr (result_always_good_v<Result>) {
+        st = r.get_state();
+        return true;
+    } else {
+        if (r) {
+            st = r.get_state();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 /**
  * @brief Attempts to apply a parser, placing the result in r. On success,
  * returns true and updates the state. On failure, returns false, and the state
@@ -67,3 +81,46 @@ constexpr bool parse_assign_value(state_t& st, P const& p, Value& v) // <br>
     }
 }
 } // namespace noam
+
+namespace noam::meta::impl {
+template <class Type, class List>
+struct prepend;
+
+template <class T, template <class...> class List, class... T2>
+struct prepend<T, List<T2...>> {
+    using type = List<T, T2...>;
+};
+
+template <template <class...> class List, class...>
+struct all_but_last;
+
+template <template <class...> class List, class A>
+struct all_but_last<List, A> {
+    using type = List<>;
+};
+template <template <class...> class List, class A, class... B>
+struct all_but_last<List, A, B...> {
+    using type =
+        typename prepend<A, typename all_but_last<List, B...>::type>::type;
+};
+
+template <class, class... B>
+struct last {
+    using type = typename last<B...>::type;
+};
+template <class A>
+struct last<A> {
+    using type = A;
+};
+} // namespace noam::meta::internal
+
+namespace noam::meta {
+template <class Type, class TypeList>
+using prepend_t = typename ::noam::meta::impl::prepend<Type, TypeList>::type;
+
+template <template <class...> class List, class... T>
+using all_but_last_t = typename ::noam::meta::impl::all_but_last<List, T...>::type;
+
+template <class... T>
+using last_t = typename ::noam::meta::impl::last<T...>::type;
+} // namespace noam::meta
