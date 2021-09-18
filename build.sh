b83d81c7ab4b7bd500ad195ef90b7a8f076a2464
Alecto Irene Perez <perez.cs@pm.me>
2021-09-17 23:52:50 -0600

    Updated build.sh to use presets

--- build.sh
+++ build.sh
@@ -3,12 +3,8 @@
 # This gets the directory of build.sh
 # It should be in the project root
 # This allows us to run build.sh from any directory, not just the project root
-project_root="$(dirname $0)"
-
-# Create build folder in project root
-mkdir -p "$project_root/build"
-cd "$project_root/build"
+cd "$(dirname "$0")"
 
 # build project in $project_root/build
-cmake .. -DCMAKE_BUILD_TYPE=Release "${@}"
-cmake --build . --config Release
+cmake --preset=default "${@}"
+cmake --build --preset=default
