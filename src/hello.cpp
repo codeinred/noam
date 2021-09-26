436825250593250e5ee57cc2397ef359460f5076
Alecto Irene Perez <perez.cs@pm.me>
2021-09-26 17:54:27 -0600

    Updated src/hello.cpp
    This commit updates the formatting of src/hello.cpp in accordance with
    the new clang-format file added in commit 201ee9b1c9
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,16 +1,16 @@
+#include <fmt/ranges.h>
 #include <iostream>
 #include <noam/co_await.hpp>
 #include <noam/combinators.hpp>
 #include <noam/intrinsics.hpp>
-#include <vector>
 #include <noam/util/fmt.hpp>
-#include <fmt/ranges.h>
+#include <vector>
 
 int main() {
     constexpr auto parser = noam::sequence<",">(noam::parse_int);
     noam::state_t str = "10, 20, 30, 40, hello";
 
-    if (auto result =  parser.parse(str)) {
+    if (auto result = parser.parse(str)) {
         std::cout << "Success" << '\n';
         fmt::print("Success\n");
         fmt::print("State: '{}'\n", result.get_state());
