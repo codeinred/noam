7db78e1960d86c8a8e58f2cdef66b2d1396d70fa
Alecto Irene Perez <perez.cs@pm.me>
2021-09-26 17:51:08 -0600

    Updated noam/util/overload_set.hpp
    This commit updates the formatting of noam/util/overload_set.hpp in
    accordance with the new clang-format file added in commit 201ee9b1c9
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/noam/util/overload_set.hpp
+++ include/noam/util/overload_set.hpp
@@ -5,7 +5,8 @@ namespace noam {
  * @brief overload_set<F...> is a functor whose overload set is the union of the
  * overload sets of { F::operator()... }
  *
- * @tparam Functions Defines the types of the functors used to construct this overload set
+ * @tparam Functions Defines the types of the functors used to construct this
+ * overload set
  */
 template <class... Functions>
 struct overload_set : Functions... {
