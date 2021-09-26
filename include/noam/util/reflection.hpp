3af8e92f6f0f59e445d7e267deb9170eac4ab58d
Alecto Irene Perez <perez.cs@pm.me>
2021-09-26 17:51:08 -0600

    Updated noam/util/reflection.hpp
    This commit updates the formatting of noam/util/reflection.hpp in
    accordance with the new clang-format file added in commit 201ee9b1c9
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/noam/util/reflection.hpp
+++ include/noam/util/reflection.hpp
@@ -4,27 +4,27 @@
 namespace noam {
 template <class T>
 constexpr std::string_view get_name_of_type() {
-    #ifdef __GNUC__
+#ifdef __GNUC__
     // GCC: constexpr name_of_type get_name_of_type() [with T = int]
     // Clang
     constexpr std::string_view name = __PRETTY_FUNCTION__;
     size_t start = name.find("T = ") + 4;
-    #if defined(__clang__) || defined(__INTEL_COMPILER)
+#if defined(__clang__) || defined(__INTEL_COMPILER)
     size_t end = name.rfind("]");
-    #else
+#else
     size_t end = name.rfind("; std::string_view = ");
-    #endif
+#endif
     return name.substr(start, end - start);
-    #elif _MSC_VER
+#elif _MSC_VER
     constexpr std::string_view name = __FUNCSIG__;
     std::string_view start_token = "get_name_of_type<";
     size_t start = name.find(start_token) + start_token.size();
     size_t end = name.rfind(">(void)");
     return name.substr(start, end - start);
-    #else
+#else
     return "<some type>"
-    #endif
+#endif
 };
 template <class T>
 constexpr std::string_view name_of_type = get_name_of_type<T>();
-}
+} // namespace noam
